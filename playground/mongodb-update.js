e163957a26848f5c7a3bb3d7b4a2bb21f4fd9f86
Adam Bohannon <abo46n2@gmail.com>
2017-10-09 17:03:10 -0700

    Added update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59dc08815e3ad83b26716be7')
+  }, {
+    $set: {
+      name: 'Adam'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  })
+
+  // db.close();
+});
